1535ca21d8156aa4fa3fdcfeef98ae8b5fd99752
Alex Sarene <sarene.alex@gmail.com>
2020-11-05 09:17:28 -0500

    Part two

--- BMICalc/MainWindow.xaml.cs
+++ BMICalc/MainWindow.xaml.cs
@@ -20,11 +20,25 @@ namespace BMICalc
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public class Customer
+        {
+            public string lastName { get; set; }
+            public string firstName { get; set; }
+            public string phoneNumber { get; set; }
+            public int heightInches { get; set; }
+            public int weightLbs { get; set; }
+            public int custBMI { get; set; }
+            public string statusTitle { get; set; }
+        }
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        #region Part 1 of lab. ClearBtn & ExitBtn
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
             xPhoneBox.Text = "";
@@ -37,6 +51,55 @@ namespace BMICalc
         private void ExitBtn_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
+        }
+        #endregion
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.firstName = xFirstNameBox.Text;
+            customer1.lastName = xLastNameBox.Text;
+            customer1.phoneNumber = xPhoneBox.Text;
+            customer1.heightInches = Convert.ToInt32(xHeightInchesBox.Text);
+            customer1.weightLbs = Convert.ToInt32(xWeightLbsBox.Text);
+
+            customer1.custBMI = BMIFormula(customer1.weightLbs, customer1.heightInches);
+
+            string yourBMIStatus = "NA";
+            customer1.statusTitle = yourBMIStatus;
+
+            //MessageBox.Show($"The Customer's name is {customer1.firstName} {customer1.lastName} and they can be reached at {customer1.phoneNumber}. They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {customer1.custBMI}");
+
+            xYourBMIResults.Text = $"{customer1.custBMI}";
+
+            if (customer1.custBMI < 18.5)
+            {
+                xBMIMessage.Text = "According to CDC.gov BMI Calculator you are underweight.";
+                customer1.statusTitle = "Underweight";
+            }
+            else if (customer1.custBMI < 24.9)
+            {
+                xBMIMessage.Text = "According to CDC.gov BMI Calculator you have a normal body weight.";
+                customer1.statusTitle = "Normal";
+            }
+            else if (customer1.custBMI < 29.9)
+            {
+                xBMIMessage.Text = "According to CDC.gov BMI Calculator you are overweight.";
+                customer1.statusTitle = "Overweight";
+            }
+            else
+            {
+                xBMIMessage.Text = "According to CDC.gov BMI Calculator you are obese.";
+                customer1.statusTitle = "Obese";
+            }
+        }
+
+        public int BMIFormula(int weight, int height)
+        {
+            int bmi;
+            bmi = 703 * weight / (height * height);
+            return bmi;
         }
     }
 }
